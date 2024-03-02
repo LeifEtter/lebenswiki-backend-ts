0ae479df1ed5d1e3d13b561969671ccca881ae71
Leif Etter <leif.etter@gmx.de>
2024-03-02 21:52:51 +0100

    add package and command for copying views

--- build/components/category/helpers.category.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertCategoryForResponse = void 0;
-const convertCategoryForResponse = (category) => ({
-    id: category.id,
-    name: category.categoryName,
-});
-exports.convertCategoryForResponse = convertCategoryForResponse;
