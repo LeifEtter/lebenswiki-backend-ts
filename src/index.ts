c81ebcdba1e9995f05d1b98e163b76bf30b20c14
Leif Etter <leif.etter@gmx.de>
2024-04-17 17:16:19 +0200

    extract app from index file into seperate app.ts file, for easier testing

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import dotenv = require("dotenv");
-dotenv.config();
-import express = require("express");
-// import cors = require("cors");
-import bodyParser = require("body-parser");
+import app from "./app";
+import db from "./database/database";
 import https = require("https");
 import fs = require("fs");
-import userRouter from "./components/user/router.user";
-import packRouter from "./components/pack/router.pack";
-import blockRouter from "./components/block/router.block";
-import commentRouter from "./components/comment/router.comment";
-import categoryRouter from "./components/category/router.category";
-import imageRouter from "./components/image/router.image";
-import shortRouter from "./components/short/router.short";
-import feedbackRouter from "./components/feedback/router.feedback";
-import logRouter from "./components/log/router.log";
-import db = require("./database/database");
-import path = require("path");
-// import corsOptionsDelegate = require("./config/cors.config");
-
-const app = express();
-app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "views"));
-app.set("views", `${__dirname}/views`);
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.json());
-app.route("/test").get((req, res: express.Response) => {
-  return res.status(200).send({ message: "Working" });
-});
-app.route("/view/deleteAccount").get((req, res) => {
-  res.render("a");
-});
-app.use("/user", userRouter);
-app.use("/pack", packRouter);
-app.use("/block", blockRouter);
-app.use("/comment", commentRouter);
-app.use("/category", categoryRouter);
-app.use("/image", imageRouter);
-app.use("/short", shortRouter);
-app.use("/feedback", feedbackRouter);
-app.use("/log", logRouter);
-// app.use(cors(corsOptionsDelegate));
 
 async function main() {
   try {
