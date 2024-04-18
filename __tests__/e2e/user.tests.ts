318cc53d3708ef60fd7e08c561bb2b17dacdf6e8
Leif Etter <leif.etter@gmx.de>
2024-04-18 21:52:52 +0200

    extract generated name and email and add test for login

--- __tests__/e2e/user.tests.ts
+++ __tests__/e2e/user.tests.ts
@@ -1,18 +1,32 @@
 import supertest from "supertest";
 import app from "../../src/app";
+import db from "../../src/database/database";
 
 const randomString = () => (Math.random() + 1).toString(36).substring(7);
 
-describe("when user signs up for an account", () => {
-  it("should respond with a success status", async () => {
-    const response = await supertest(app)
-      .post("/user/register")
-      .send({
-        name: `${randomString()} ${randomString()}`,
-        email: `${randomString()}@lebenswiki.com`,
-        password: "Test@1234",
-        biography: "Lorem Ipsum dolor sit amed",
-      });
-    expect(response.statusCode == 201);
+const name = randomString();
+const email = randomString() + "." + randomString() + "@lebenswiki.com";
+const password = "Test@123456";
+
+describe("the user login and registration process", () => {
+  describe("when the user signs up with valid inputs", () => {
+    it("should respond with a success status on login", async () => {
+      await supertest(app)
+        .post("/user/register")
+        .send({
+          name: name,
+          email: email,
+          password: password,
+          biography: "Lorem Ipsum dolor sit amed",
+        })
+        .expect(201);
+    });
+
+    it("should respond with successful status code on registration", async () => {
+      await supertest(app)
+        .post("/user/login")
+        .send({ email: email, password: password })
+        .expect(200);
+    });
   });
 });
