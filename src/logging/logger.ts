7147f0e8ea1efbdf8546c4fc691b2976f1b3371a
Leif Etter <leif.etter@gmx.de>
2024-04-25 20:23:16 +0200

    enable logging to console and file

--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,5 +1,18 @@
 import pino from "pino";
 
-const logger = pino({});
+const fileTransport = pino.transport({
+  targets: [
+    {
+      target: "pino-pretty",
+      options: {},
+    },
+    {
+      target: "pino/file",
+      options: { destination: `${__dirname}/app.log` },
+    },
+  ],
+});
+
+const logger = pino(fileTransport);
 
 export default logger;
