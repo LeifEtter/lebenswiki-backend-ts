6e1e97475391af352cda1b82b43913a9baeda277
Leif Etter <leif.etter@gmx.de>
2024-04-25 20:45:48 +0200

    add build folder to .gitignore and untrack

--- build/constants/errorMessages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GENERIC_LOGIN_ERROR = exports.GENERIC_REGISTRATION_ERROR = exports.GENERIC_AUTH_ERROR = void 0;
-exports.GENERIC_AUTH_ERROR = "Something went wrong during Authentication, please ensure you are passing the required Authorization Header";
-exports.GENERIC_REGISTRATION_ERROR = "User couldn't be registered, please try again later";
-exports.GENERIC_LOGIN_ERROR = "Email Address or Password was invalid";
-// export const GENERIC_COMMENT_ERROR = "Couldn't find a"
